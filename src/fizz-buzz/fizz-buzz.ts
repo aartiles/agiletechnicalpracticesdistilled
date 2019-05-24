b151e9635c86b174f316726e4e688ad2dbfa3bdd
Alfredo Artiles <aartiles@gmail.com>
2019-05-24 20:50:12 +0200

    fizzbuzz of 1 should be "1"

---
+++ src/fizz-buzz/fizz-buzz.ts
@@ -0,0 +1,3 @@
+export default function fizzbuzz(n: number): string {
+  return '1';
+}
--- tests/fizz-buzz/fizz-buzz_spec.ts
+++ tests/fizz-buzz/fizz-buzz_spec.ts
@@ -1,6 +1,9 @@
 import should from 'should';
+import fizzbuzz from '../../src/fizz-buzz/fizz-buzz';
 
 describe('Fizz Buzz', () => {
-  it('', () => {
+  it('fizzbuzz of 1 should be "1"', () => {
+    should(fizzbuzz(1)).be.eql('1');
   });
+
 });

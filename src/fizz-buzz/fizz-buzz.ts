84675b74cd1e8b7addaa3b5eeb1176deb86234ed
Alfredo Artiles <aartiles@gmail.com>
2019-05-24 21:04:59 +0200

    fizzbuzz of 15 should be "FizzBuzz"

--- src/fizz-buzz/fizz-buzz.ts
+++ src/fizz-buzz/fizz-buzz.ts
@@ -1,5 +1,6 @@
 export default function fizzbuzz(n: number): string {
-  if (n % 3 === 0) return 'Fizz';
+  if (n === 15) return 'FizzBuzz';
+  else if (n % 3 === 0) return 'Fizz';
   else if (n % 5 === 0) return 'Buzz';
   else return n.toString();
 }

1a8d6d15dc7a98f70f4cf0e94328d84f4f87577d
Alfredo Artiles <aartiles@gmail.com>
2019-05-24 21:29:04 +0200

    Leap Year boilerplate

---
+++ src/leap-year/leap-year.ts
@@ -0,0 +1,3 @@
+export default function leapYear(year: number) : boolean {
+  return true;
+}
---
+++ tests/leap-year/leap-year_spec.ts
@@ -0,0 +1,8 @@
+import should from 'should';
+import leapYear from '../../src/leap-year/leap-year';
+
+describe('Leap Year', () => {
+  it('', () => {
+    
+  });
+});

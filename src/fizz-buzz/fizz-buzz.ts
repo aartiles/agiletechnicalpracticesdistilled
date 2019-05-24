09c9b3cc7071d224dbf24550daf413d4ce4ede12
Alfredo Artiles <aartiles@gmail.com>
2019-05-24 21:09:41 +0200

    fizzbuzz for multiple of 3 and 5 is "FizzBuzz"

--- src/fizz-buzz/fizz-buzz.ts
+++ src/fizz-buzz/fizz-buzz.ts
@@ -1,5 +1,5 @@
 export default function fizzbuzz(n: number): string {
-  if (n === 15) return 'FizzBuzz';
+  if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
   else if (n % 3 === 0) return 'Fizz';
   else if (n % 5 === 0) return 'Buzz';
   else return n.toString();

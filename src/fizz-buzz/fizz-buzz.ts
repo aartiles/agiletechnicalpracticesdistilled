dedd4110b2e2842f0b67f8077e14f02bde546b45
Alfredo Artiles <aartiles@gmail.com>
2019-05-24 21:02:28 +0200

    fizzbuzz of multiple of 5 should be "Buzz"

--- src/fizz-buzz/fizz-buzz.ts
+++ src/fizz-buzz/fizz-buzz.ts
@@ -1,5 +1,5 @@
 export default function fizzbuzz(n: number): string {
   if (n % 3 === 0) return 'Fizz';
-  else if (n === 5) return 'Buzz';
+  else if (n % 5 === 0) return 'Buzz';
   else return n.toString();
 }

5ea1d9295a3e6762c06b2c1b2c4bff943f15bbb0
Alfredo Artiles <aartiles@gmail.com>
2019-05-24 21:25:18 +0200

    fizzbuzz should not accept a number lower than 1

--- src/fizz-buzz/fizz-buzz.ts
+++ src/fizz-buzz/fizz-buzz.ts
@@ -1,4 +1,5 @@
 export default function fizzbuzz(n: number): string {
+  if (n < 1) throw new Error('n should be bigger than 0');
   if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
   else if (n % 3 === 0) return 'Fizz';
   else if (n % 5 === 0) return 'Buzz';

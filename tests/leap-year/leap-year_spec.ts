1ab59376ef17e1a37b8b05cb3a92f03f0bdebe09
Alfredo Artiles <aartiles@gmail.com>
2019-05-29 20:19:31 +0200

    detect typical leap years

--- tests/leap-year/leap-year_spec.ts
+++ tests/leap-year/leap-year_spec.ts
@@ -2,7 +2,7 @@ import should from 'should';
 import leapYear from '../../src/leap-year/leap-year';
 
 describe('Leap Year', () => {
-  it('', () => {
-    
+  it('detect typical leap years', () => {
+    leapYear(1966).should.be.true();
   });
 });

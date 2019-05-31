9af434a7e75c184075ac7d0517014a5a17aef120
Alfredo Artiles <aartiles@gmail.com>
2019-05-31 18:51:01 +0200

    Extract addWord logic

--- src/anagrams/anagrams.ts
+++ src/anagrams/anagrams.ts
@@ -1,11 +1,18 @@
 export default function anagrams(s: string): Array<string> {
-  const res = [s];
+  const words = [s];
   if (s.length <= 2) {
-    const newWord = swapChars(s, 0, 1);
-    if (res.indexOf(newWord) < 0) res.push(newWord);
-    return res;
+    addWord(words, s, 0, 1);
+    return words;
   }
-  else return [s,  swapChars(s, 1, 2), swapChars(s, 0, 2)];
+  else {
+
+    return [s,  swapChars(s, 1, 2), swapChars(s, 0, 2)];
+  }
+}
+
+function addWord(words: Array<string>, s: string, pos1: number, pos2: number): void {
+  const newWord = swapChars(s, 0, 1);
+  if (words.indexOf(newWord) < 0) words.push(newWord);
 }
 
 function swapChars(s: string, pos1: number, pos2: number): string {

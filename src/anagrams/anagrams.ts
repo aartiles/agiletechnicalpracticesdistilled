438ed69978f4f68d2637febbff8bbf04a6f17663
Alfredo Artiles <aartiles@gmail.com>
2019-05-31 18:46:15 +0200

    Refactor 2 chars case

--- src/anagrams/anagrams.ts
+++ src/anagrams/anagrams.ts
@@ -1,13 +1,15 @@
 export default function anagrams(s: string): Array<string> {
-  if (s.length <= 1) return [s];
-  else if (s.length === 2) {
-    if (s[0] === s[1]) return [s];
-    else return [s, swapChars(s, 0, 1)];
+  const res = [s];
+  if (s.length <= 2) {
+    const newWord = swapChars(s, 0, 1);
+    if (res.indexOf(newWord) < 0) res.push(newWord);
+    return res;
   }
   else return [s,  swapChars(s, 1, 2), swapChars(s, 0, 2)];
 }
 
 function swapChars(s: string, pos1: number, pos2: number): string {
+  if (pos2 > s.length - 1) return s;
   return s.substring(0, pos1) +
     s[pos2] +
     s.substr(pos1 + 1, pos2 - pos1 - 1) +

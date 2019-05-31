774541315126cb4109a643e767a2c2c91a9807c3
Alfredo Artiles <aartiles@gmail.com>
2019-05-31 20:51:16 +0200

    Refactor

--- src/roman-numerals/roman-numerals.ts
+++ src/roman-numerals/roman-numerals.ts
@@ -1,5 +1,3 @@
 export default function romanNumeral(n: number): string {
-  if (n === 1) return 'I';
-  else if (n === 2) return 'II';
-  else if (n === 3) return 'III';
+  return Array(n).fill('I').join('');
 }

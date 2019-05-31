9a4d02eb16fad5fe5aa588167711df2276185428
Alfredo Artiles <aartiles@gmail.com>
2019-05-31 16:30:28 +0200

    Anagrams boilerplate

---
+++ src/anagrams/anagrams.ts
@@ -0,0 +1,3 @@
+export default function anagrams(s: string): string {
+  return '';
+}
---
+++ tests/anagrams/anagrams_spec.ts
@@ -0,0 +1,7 @@
+import should from 'should';
+import anagrams from '../../src/anagrams/anagrams';
+
+describe('Anagrams', () => {
+  it('', () => {
+  });
+});

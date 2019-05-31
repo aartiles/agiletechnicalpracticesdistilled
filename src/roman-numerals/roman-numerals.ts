c7d95004ecbddb720b04d64b809d0fc479663820
Alfredo Artiles <aartiles@gmail.com>
2019-05-31 21:00:14 +0200

    Refactor filling

--- src/roman-numerals/roman-numerals.ts
+++ src/roman-numerals/roman-numerals.ts
@@ -1,5 +1,9 @@
 export default function romanNumeral(n: number): string {
-  if (n <= 3) return Array(n).fill('I').join('');
+  if (n <= 3) return fill(n, 'I');
   else if (n === 4) return 'IV';
-  else if (n >= 5) return 'V' + Array(n - 5).fill('I').join('');
+  else if (n >= 5) return `V${fill(n - 5, 'I')}`;
 }
+
+function fill(n: number, char: string): string {
+  return Array(n).fill(char).join('');
+} 

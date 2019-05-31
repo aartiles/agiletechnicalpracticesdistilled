5c3d95331934097638f72c1e1c9d70df48728918
Alfredo Artiles <aartiles@gmail.com>
2019-05-31 20:34:42 +0200

    Roman numerals boilerplate

---
+++ src/roman-numerals/roman-numerals.ts
@@ -0,0 +1,3 @@
+export default romanNumeral(n: number): string {
+  return '';
+}
---
+++ tests/roman-numerals/roman-numerals_spec.ts
@@ -0,0 +1,7 @@
+import should from 'should';
+import romanNumeral from '../../src/roman-numerals/roman-numerals';
+
+describe('Roman Numerals', () => {
+  it('foo', () => {
+  });
+});

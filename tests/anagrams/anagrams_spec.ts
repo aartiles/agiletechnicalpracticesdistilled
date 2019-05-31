f48bc55a73da935b84bcfa8a6aa60bf6b3c893d5
Alfredo Artiles <aartiles@gmail.com>
2019-05-31 17:05:10 +0200

    test that anagram of a a 2 characters word is the same and other with characters interchanges

--- tests/anagrams/anagrams_spec.ts
+++ tests/anagrams/anagrams_spec.ts
@@ -6,4 +6,12 @@ describe('Anagrams', () => {
     anagrams('a').should.be.eql(['a']);
   });
 
+  it('anagram of a the same charactes is the same word', () => {
+    anagrams('aa').should.be.eql(['aa']);
+  });
+
+  it('anagram of a a 2 characters word is the same and other with characters interchanges', () => {
+    anagrams('ab').should.be.eql(['ab', 'ba']);
+  });
+
 });

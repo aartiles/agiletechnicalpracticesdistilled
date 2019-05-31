983fb16af0bb772bffa285b337d8d527fd0b8750
Alfredo Artiles <aartiles@gmail.com>
2019-05-31 19:53:51 +0200

    Extract single word combination

--- src/anagrams/anagrams.ts
+++ src/anagrams/anagrams.ts
@@ -7,12 +7,18 @@ export default function anagrams(s: string): Array<string> {
 function combineWords(words: Array<string>): boolean {
   let foundNew = false;
   words.forEach((word) => {
-    for (let pos1 = 0; pos1 < word.length - 1; pos1++) {
-      for (let pos2 = pos1 + 1; pos2 < word.length; pos2++) {
-        foundNew = foundNew || addWord(words, word, pos1, pos2);
-      }
-    }
+    foundNew = foundNew || combineWord(words, word);
   });
+  return foundNew;
+}
+
+function combineWord(words: Array<string>, word: string): boolean {
+  let foundNew = false;
+  for (let pos1 = 0; pos1 < word.length - 1; pos1++) {
+    for (let pos2 = pos1 + 1; pos2 < word.length; pos2++) {
+      foundNew = foundNew || addWord(words, word, pos1, pos2);
+    }
+  }
   return foundNew;
 }
 

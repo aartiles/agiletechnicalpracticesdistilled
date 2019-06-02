5a49542f32211f3faf974189b3cb416c0ca9bbfb
Alfredo Artiles <aartiles@gmail.com>
2019-06-02 20:12:08 +0200

    Refactor

--- src/roman-numerals-tpp/roman-numerals.ts
+++ src/roman-numerals-tpp/roman-numerals.ts
@@ -5,7 +5,7 @@ export default function romanNumeral(n: number): string {
   else if (n === 5) return 'V';
   else if (n > 5 && n < 9) return romanNumeral(5) + romanNumeral(n - 5);
   else if (n === 10) return 'X';
-  else if (n > 10 && n < 40) return 'X' + romanNumeral(n-10);
+  else if (n > 10 && n < 40) return romanNumeral(10) + romanNumeral(n-10);
   else if (n === 40) return 'XL';
   else if (n === 50) return 'L';
 }

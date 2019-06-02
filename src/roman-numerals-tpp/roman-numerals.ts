ca0ef375053af34072e1ceb9ac7e2c62ee24bc8a
Alfredo Artiles <aartiles@gmail.com>
2019-06-02 19:10:41 +0200

    Refactor to tail recursion

--- src/roman-numerals-tpp/roman-numerals.ts
+++ src/roman-numerals-tpp/roman-numerals.ts
@@ -3,8 +3,8 @@ export default function romanNumeral(n: number): string {
   if (n <= 3) return Array(n).fill('I').join('');
   else if (n === 4) return 'IV';
   else if (n === 5) return 'V';
-  else if (n > 5 && n < 9) return 'V' + Array(n - 5).fill('I').join('');
+  else if (n > 5 && n < 9) return 'V' + romanNumeral(n-5);
   else if (n === 9) return 'IX';
   else if (n === 10) return 'X';
-  else if (n > 10) return 'X' + Array(n - 10).fill('I').join('');;
+  else if (n > 10) return 'X' + romanNumeral(n-10);
 }

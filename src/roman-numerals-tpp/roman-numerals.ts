751bd72be6547e66a39f83965ac97e035372d179
Alfredo Artiles <aartiles@gmail.com>
2019-06-02 19:01:38 +0200

    roman of 7 is VII'

--- src/roman-numerals-tpp/roman-numerals.ts
+++ src/roman-numerals-tpp/roman-numerals.ts
@@ -3,5 +3,5 @@ export default function romanNumeral(n: number): string {
   if (n <= 3) return Array(n).fill('I').join('');
   else if (n === 4) return 'IV';
   else if (n === 5) return 'V';
-  else if (n === 6) return 'VI';
+  else if (n > 5) return 'V' + Array(n - 5).fill('I').join('');
 }

e854196240f5e1e623a561e8c7cba07eca33eb25
Alfredo Artiles <aartiles@gmail.com>
2019-06-02 18:50:17 +0200

    test that oman of 1 is I

---
+++ src/roman-numerals-tpp/roman-numerals.ts
@@ -0,0 +1,4 @@
+
+export default function romanNumeral(n: number): string {
+  return ''; 
+}
---
+++ tests/roman-numerals-tpp/roman-numerals_spec.ts
@@ -0,0 +1,8 @@
+import should from 'should';
+import romanNumeral from '../../src/roman-numerals-tpp/roman-numerals';
+
+describe('Roman Numerals', () => {
+  it('roman of 1 is I', () => {
+    romanNumeral(1).should.be.eql('I');
+  });
+});

7d30ba0df1ad407a0c6b08d5db90488da8086480
Alfredo Artiles <aartiles@gmail.com>
2019-06-02 18:51:46 +0200

    test that roman of 2 is II

--- tests/roman-numerals-tpp/roman-numerals_spec.ts
+++ tests/roman-numerals-tpp/roman-numerals_spec.ts
@@ -5,4 +5,9 @@ describe('Roman Numerals', () => {
   it('roman of 1 is I', () => {
     romanNumeral(1).should.be.eql('I');
   });
+
+  it('roman of 2 is II', () => {
+    romanNumeral(2).should.be.eql('II');
+  });
+
 });

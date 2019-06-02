d3124a3759b4d386b960f04756cd9724f861ebfb
Alfredo Artiles <aartiles@gmail.com>
2019-06-02 19:03:58 +0200

    roman of 9 is IX

--- src/roman-numerals-tpp/roman-numerals.ts
+++ src/roman-numerals-tpp/roman-numerals.ts
@@ -3,5 +3,6 @@ export default function romanNumeral(n: number): string {
   if (n <= 3) return Array(n).fill('I').join('');
   else if (n === 4) return 'IV';
   else if (n === 5) return 'V';
-  else if (n > 5) return 'V' + Array(n - 5).fill('I').join('');
+  else if (n > 5 && n < 9) return 'V' + Array(n - 5).fill('I').join('');
+  else if (n === 9) return 'IX';
 }

8403d791051c8bfb3345f54a365822fefa4b022e
Alfredo Artiles <aartiles@gmail.com>
2019-06-02 19:00:13 +0200

    roman of 6 is VI

--- src/roman-numerals-tpp/roman-numerals.ts
+++ src/roman-numerals-tpp/roman-numerals.ts
@@ -3,4 +3,5 @@ export default function romanNumeral(n: number): string {
   if (n <= 3) return Array(n).fill('I').join('');
   else if (n === 4) return 'IV';
   else if (n === 5) return 'V';
+  else if (n === 6) return 'VI';
 }

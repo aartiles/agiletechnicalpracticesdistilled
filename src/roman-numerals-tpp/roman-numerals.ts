0b974b1ec945a471076679cba5b83add714f8fe3
Alfredo Artiles <aartiles@gmail.com>
2019-06-03 19:48:50 +0200

    roman of 60 is L

--- src/roman-numerals-tpp/roman-numerals.ts
+++ src/roman-numerals-tpp/roman-numerals.ts
@@ -5,7 +5,8 @@ export default function romanNumeral(n: number): string {
   else if (n === 5) return 'V';
   else if (n > 5 && n < 9) return romanNumeral(5) + romanNumeral(n - 5);
   else if (n === 10) return 'X';
-  else if (n > 10 && n < 40) return romanNumeral(10) + romanNumeral(n-10);
+  else if (n > 10 && n < 40) return romanNumeral(10) + romanNumeral(n - 10);
   else if (n === 40) return romanNumeral(n / 4) + romanNumeral((n + 10));
   else if (n === 50) return 'L';
+  else if (n === 60) return romanNumeral(50) + romanNumeral(n - 50);
 }

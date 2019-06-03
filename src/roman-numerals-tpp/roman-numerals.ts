e0ff464cc5d73e9ceeb9b1c4056b63904fd6b49d
Alfredo Artiles <aartiles@gmail.com>
2019-06-03 20:33:28 +0200

    Refactor

--- src/roman-numerals-tpp/roman-numerals.ts
+++ src/roman-numerals-tpp/roman-numerals.ts
@@ -8,7 +8,6 @@ export default function romanNumeral(n: number): string {
   else if (n <= 3) return Array(n).fill('I').join('');
   else if (n === 4 || n == 9) return romanNumeral(1) + romanNumeral(n + 1);
   else if (n > 5 && n < 9) return romanNumeral(5) + romanNumeral(n - 5);
-  else if (isBase10(n)) return BASE10[Math.log10(n)];
   else if (n > 10 && n < 40) return romanNumeral(10) + romanNumeral(n - 10);
   else if (n === 40) return romanNumeral(n / 4) + romanNumeral((n + 10));
   else if (n >= 60 && n < 90) return romanNumeral(50) + romanNumeral(n - 50);

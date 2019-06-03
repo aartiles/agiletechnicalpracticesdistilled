c1844a4931b377e4f93323862e634d04b9a375b4
Alfredo Artiles <aartiles@gmail.com>
2019-06-03 20:33:06 +0200

    Refactor

--- src/roman-numerals-tpp/roman-numerals.ts
+++ src/roman-numerals-tpp/roman-numerals.ts
@@ -1,15 +1,16 @@
 
 const BASE10 = ['I', 'X', 'C', 'M'];
+const HALFBASE10 = ['V', 'L', 'D'];
 
 export default function romanNumeral(n: number): string {
-  if (n <= 3) return Array(n).fill('I').join('');
+  if (isBase10(n)) return BASE10[Math.log10(n)];
+  else if (isHalfBase10(n)) return HALFBASE10[Math.log10(n / 5)];
+  else if (n <= 3) return Array(n).fill('I').join('');
   else if (n === 4 || n == 9) return romanNumeral(1) + romanNumeral(n + 1);
-  else if (n === 5) return 'V';
   else if (n > 5 && n < 9) return romanNumeral(5) + romanNumeral(n - 5);
   else if (isBase10(n)) return BASE10[Math.log10(n)];
   else if (n > 10 && n < 40) return romanNumeral(10) + romanNumeral(n - 10);
   else if (n === 40) return romanNumeral(n / 4) + romanNumeral((n + 10));
-  else if (n === 50) return 'L';
   else if (n >= 60 && n < 90) return romanNumeral(50) + romanNumeral(n - 50);
   else if (n === 90) return romanNumeral(10) + romanNumeral(100);
   else if (n > 100) return romanNumeral(100) + romanNumeral(n - 100);;
@@ -19,3 +20,6 @@ function isBase10(n: number): boolean {
   return Number.isInteger(Math.log10(n));
 }
 
+function isHalfBase10(n: number): boolean {
+  return n % 5 === 0 && isBase10(n / 5);
+}

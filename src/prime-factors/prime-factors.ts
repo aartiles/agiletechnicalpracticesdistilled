14cf5ecb2d41ccff42a1847b377594f0e4f1d356
Alfredo Artiles <aartiles@gmail.com>
2019-06-07 17:25:13 +0200

    Refactor

--- src/prime-factors/prime-factors.ts
+++ src/prime-factors/prime-factors.ts
@@ -1,10 +1,8 @@
 export default function primeFactors(n: number): Array<number> {
-  if (n < 2) return [];
-  else {
-    const factor = nextFactor(n);
-    if (factor === 1) return [n];
-    else return primeFactors(n / factor).concat(primeFactors(factor));
-  }
+  const factor = nextFactor(n);
+  if (factor === 0) return [];
+  else if (factor === 1) return [n];
+  else return primeFactors(n / factor).concat(primeFactors(factor));
 }
 
 function nextFactor(n: number): number {

7baddb50be8f9c6bdf666536cb4987a765d4f89f
Alfredo Artiles <aartiles@gmail.com>
2019-06-07 16:33:48 +0200

    Test that prime factors of 5 are 2 and 3

--- tests/prime-factors/prime-factors_spec.ts
+++ tests/prime-factors/prime-factors_spec.ts
@@ -2,9 +2,10 @@ import should from 'should';
 import primeFactors from '../../src/prime-factors/prime-factors';
 
 describe('Prime Factors', () => {
-  it('foo', () => {
+  it('computes prime factors correctly', () => {
     primeFactors(2).should.be.eql([2]);
     primeFactors(3).should.be.eql([3]);
     primeFactors(4).should.be.eql([2, 2]);
+    primeFactors(5).should.be.eql([2, 3]);
   });
 });

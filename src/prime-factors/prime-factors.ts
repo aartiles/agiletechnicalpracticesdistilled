1d2d9fce72af3fbf2ea5816293f474ca81d0e03d
Alfredo Artiles <aartiles@gmail.com>
2019-06-07 16:40:18 +0200

    Transform to tail recursion

--- src/prime-factors/prime-factors.ts
+++ src/prime-factors/prime-factors.ts
@@ -1,6 +1,6 @@
 export default function primeFactors(n: number): Array<number> {
   if (n <= 3) return [n];
-  else if (n === 4) return [2, 2];
-  else if (n === 6) return [2, 3];
-  else if (n === 9) return [3, 3];
+  else if (n === 4) return primeFactors(2).concat(primeFactors(2));
+  else if (n === 6) return primeFactors(2).concat(primeFactors(3));
+  else if (n === 9) return primeFactors(3).concat(primeFactors(3));
 }

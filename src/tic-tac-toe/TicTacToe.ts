9b30dd57a2fa82198741b2b11a009f19297a28c9
Alfredo Artiles <aartiles@gmail.com>
2019-06-07 18:04:41 +0200

    Place X correctly at 1,1

--- src/tic-tac-toe/TicTacToe.ts
+++ src/tic-tac-toe/TicTacToe.ts
@@ -1,5 +1,13 @@
 export default class TicTacToe {
   play(player: string, x: number, y: number) {
-    throw new Error('Not your turn');
+    if (player === 'O') throw new Error('Not your turn');
+  }
+
+  render(): Array<Array<string>> {
+    return [
+      ['X', '-', '-'],
+      ['-', '-', '-'],
+      ['-', '-', '-']
+    ];
   }
 }
--- tests/tic-tac-toe/TicTacToe_spec.ts
+++ tests/tic-tac-toe/TicTacToe_spec.ts
@@ -8,5 +8,15 @@ describe.only('Tic Tac Toe Game', () => {
       game.play('O', 1, 1);
     }).throw('Not your turn');
   });
-  
+
+  it('Place X correctly at 1,1', () => {
+    const game = new TicTacToe();
+    game.play('X', 1, 1);
+    game.render().should.be.eql([
+      ['X', '-', '-'],
+      ['-', '-', '-'],
+      ['-', '-', '-']
+    ]);
+  });
+
 });

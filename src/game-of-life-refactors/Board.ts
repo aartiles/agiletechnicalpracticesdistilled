d84c6975f599423bd9f099066e4b85250cc1fb49
Alfredo Artiles <aartiles@gmail.com>
2019-07-27 12:20:27 +0200

    Extract board

---
+++ src/game-of-life-refactors/Board.ts
@@ -0,0 +1,54 @@
+import Cell from "./Cell";
+import Position from "./Position";
+
+export default class Board {
+  private cells: Array<Cell>;
+
+  constructor() {
+    this.cells = [];
+  }
+
+  add(cell: Cell) {
+    this.cells.push(cell);
+  }
+
+  countLiveNeighbors(position: Position): number {
+    return this.cells.reduce((liveNeighbors, liveCell) => {
+      if (liveCell.isAlive() && liveCell.position().isNeighbor(position)) liveNeighbors++;
+      return liveNeighbors;
+    }, 0);
+  }
+
+  expandDeadCellsFromLiveCells() {
+    this.cells = this.cells.reduce((expandedCells, cell) => {
+      const neighbors = cell.position().getNeighbors();
+      const newDeadCells = neighbors.reduce((deadCells, neighbor) => {
+        if (!this.existsCell(expandedCells, neighbor))
+          deadCells.push(new Cell('dead', neighbor));
+        return deadCells;
+      }, []);
+      return [...expandedCells, ...newDeadCells];
+    }, this.cells);
+  }
+
+  filter(filterFn: (cell: Cell) => boolean) {
+    this.cells = this.cells.reduce((newGeneration, cell) => {
+      if (filterFn(cell)) newGeneration.push(new Cell('live', cell.position()));
+      return newGeneration;
+    }, []);
+  }
+
+  toArray(): Array<Array<number>> {
+    return this.cells.reduce((liveCells, cell) => {
+      if (cell.isAlive()) liveCells.push(cell.position().toArray());
+      return liveCells;
+    }, []);
+  }
+
+  private existsCell(cells: Array<Cell>, position: Position): boolean {
+    return cells.some(cell => {
+      return cell.position().isEqual(position);
+    });
+  }
+
+}
--- src/game-of-life-refactors/GameOfLife.ts
+++ src/game-of-life-refactors/GameOfLife.ts
@@ -1,59 +1,41 @@
 import Position from './Position';
 import Cell from './Cell';
+import Board from './Board';
 
 class GameOfLife {
-  private cells: Array<Cell>;
+
+  private board: Board;
+
   constructor(seed: Array<Array<number>>) {
-    this.cells = seed.map(
-      position => new Cell('live', new Position(position[0], position[1]))
-    );
+    this.board = new Board();
+
+    seed.forEach(([row, column]) => {
+      const cell = new Cell('live', new Position(row, column));
+      this.board.add(cell);
+    });
   }
 
   nextGeneration() {
-    const cells = this.expandDeadCellsFromLiveCells();
-    this.cells = cells.reduce(this.tryToAddCellToNextGeneration, []);
+    this.board.expandDeadCellsFromLiveCells();
+    this.board.filter(this.isAliveInNextGeneration.bind(this));
   }
 
-  private tryToAddCellToNextGeneration(newGeneration: Array<Cell>, cell: Cell): Array<Cell> {
-    const numbersOfLiveNeighbors = this.getLiveNeighbors(cell.position());
-    if (cell.isAlive() && (numbersOfLiveNeighbors < 2 || numbersOfLiveNeighbors > 3))
-      return newGeneration;
-    if (numbersOfLiveNeighbors === 2 || numbersOfLiveNeighbors === 3)
-      newGeneration.push(cell);
-    if (!cell.isAlive() && numbersOfLiveNeighbors === 3)
-      newGeneration.push(new Cell('live', cell.position()));
-    return newGeneration;
-  }
-
-  private expandDeadCellsFromLiveCells(): Array<Cell> {
-    return this.cells.reduce((expandedCells, cell) => {
-      const neighbors = cell.position().getNeighbors();
-      const newDeadCells = neighbors.reduce((deadCells, neighbor) => {
-        if (!this.existsCell(expandedCells, neighbor))
-          deadCells.push(new Cell('dead', neighbor));
-        return deadCells;
-      }, []);
-      return [...expandedCells, ...newDeadCells];
-    }, this.cells);
-  }
-
-  private existsCell(cells: Array<Cell>, position: Position): boolean {
-    return cells.some(cell => {
-      return cell.position().isEqual(position);
-    });
-  }
-  private getLiveNeighbors(position: Position): number {
-    return this.cells.reduce((liveNeighbors, liveCell) => {
-      if (liveCell.position().isNeighbor(position)) liveNeighbors++;
-      return liveNeighbors;
-    }, 0);
+  private isAliveInNextGeneration(cell: Cell): boolean {    
+    const numberOfLiveNeighbors = this.board.countLiveNeighbors(cell.position());
+    if (cell.isAlive() && (numberOfLiveNeighbors < 2 || numberOfLiveNeighbors > 3)) {
+      return false;
+    }
+    if (cell.isAlive() && (numberOfLiveNeighbors === 2 || numberOfLiveNeighbors === 3)) {
+      return true;
+    }
+    if (!cell.isAlive() && numberOfLiveNeighbors === 3) {
+      return true;
+    }
+    return false;
   }
 
   currentGeneration(): Array<Array<number>> {
-    return this.cells.reduce((liveCells, cell) => {
-      if (cell.isAlive()) liveCells.push(cell.position().toArray());
-      return liveCells;
-    }, []);
+    return this.board.toArray();
   }
 }
 
